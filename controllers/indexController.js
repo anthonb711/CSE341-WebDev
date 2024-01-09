110bfd4dc5cd1be867e4315a73a5ae358766a936
anthonb711 <anthonb711@gmail.com>
2024-01-08 22:28:50 -0500

    fixing files path issues for indexController.js

---
+++ controllers/indexController.js
@@ -0,0 +1,7 @@
+exports.kaiaName = (req, res) => {
+  res.send("Kaia Wren Brown");
+};
+
+exports.profiles = (req, res) => {
+  res.send("Hello, World from indexController.js");
+};
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const indexController = require('/..controllers/indexController');
+const indexController = require('../controllers/indexController');
 
 // home route
 router.get('/', indexController.kaiaName);
 router.post('/', indexController.kaiaName);
 
 // profile route
-router.get('/', indexController.profiles);
-router.post('/', indexController.profiles);
+router.get('/profile', indexController.profiles);
+router.post('/profile', indexController.profiles);
 
   module.exports = router;
