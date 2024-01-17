d86583bd6ad3de1609bcf055055d400a846bb08b
anthonb711 <anthonb711@gmail.com>
2024-01-16 19:35:48 -0500

    adding mongo

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
 const express = require('express'); // Must require a module to use it. Here we require the express module
 const app = express (); // app creates an instance of the express module and acts as a sudo-namespace
 const indexRoutes = require('./routes/index.js');
+const {MongoClient} = require ('mongodb');
+
+async function main() {
+  const uri = "mongodb+srv://anthonb711:Night317Walker$@cse341-contacts.2j9r783.mongodb.net/?retryWrites=true&w=majority";
+
+  const client = new MongoClient (uri);
+  try {
+    await client.connect();
+
+    await listDatabases(client);
+
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+}
+main().catch(console.error);
+
+async function listDatabases(client) {
+  const databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach(db => {
+    console.log(`- ${db.name}`);
+  });
+}
 
 app.use('/', indexRoutes);
 
