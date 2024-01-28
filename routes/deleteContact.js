04e74435808d9dd1a98b794387d390273daccf67
anthonb711 <anthonb711@gmail.com>
2024-01-27 22:48:17 -0500

    updates to comments, final commit before submission L03

--- routes/deleteContact.js
+++ routes/deleteContact.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const deleteContactController = require('../controllers/deleteContactController');
 
-router.delete('/:id', deleteContactController.deleteContact);   // updates a contact
+router.delete('/:id', deleteContactController.deleteContact);   // deletes a contact
 
 module.exports = router;
