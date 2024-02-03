83a8c92fa27a5426d407246e3138f3893c7a41b4
anthonb711 <anthonb711@gmail.com>
2024-02-03 00:38:58 -0500

    change the home get to the get contacts route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const indexController = require('../controllers/indexController');
+const contactController = require('../controllers/contactController');
 
 
-router.get('/', indexController.get          );
+router.get('/', contactController.getContacts          );
 router.use('/profile',  require('./profile') );
 router.use('/contacts', require('./contacts'));
 
